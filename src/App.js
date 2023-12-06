506145864d29dfbcac13ea1c6e7f64a1cfb654aa
Sade Joseph <feojoseph1@gmail.com>
2023-12-06 19:03:42 +0000

    compnents and navigation

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import Navigation from '../components/navigation/Navigation';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App"> 
+        <Navigation/>
+         {/*
+          <imageLinkForm/>
+          <FaceRecognition/>*/}
+
+
+
+      </div>
+      
+    );
+  }
+}
+
+export default App;
