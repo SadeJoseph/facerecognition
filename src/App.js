d24abb80e8f66ef1b772512c8f2508c8114cc406
Sade Joseph <feojoseph1@gmail.com>
2024-01-08 15:31:52 +0000

    added bit of functionality, detect button and input box

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+   console.log(event.target.value);
+  }
+  onButtonSubmit =() => {
+    console.log('click');
+  }
+
+
   render() {
     return (
       <div className="App"> 
         <Navigation/>
         <Logo />
         <Rank />
-        <ImageLinkForm/>
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}/>
         
 
 
